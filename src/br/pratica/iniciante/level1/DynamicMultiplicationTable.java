a9a1c82695b9bacc1c408d6d64092b14b12c22e3
AlexandreLopes02 <alenovoxio@gmail.com>
2025-09-19 10:07:13 -0300

    Level 1 done

--- src/br/pratica/iniciante/level1/DynamicMultiplicationTable.java
+++ src/br/pratica/iniciante/level1/DynamicMultiplicationTable.java
@@ -1,7 +1,20 @@
 package br.pratica.iniciante.level1;
 
+import java.util.Scanner;
+
 public class DynamicMultiplicationTable {
     public static void main(String[] args) {
-        System.out.println("Hello word!");
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o numero que deseja saber a tabuada↓↓↓");
+        int numero = leitura.nextInt();
+
+        System.out.println("Tabuada do " + numero + ": ");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " * " + i +" = " + resultado);
+        }
+
+        leitura.close();
     }
 }
--- src/br/pratica/iniciante/level1/SimpleCalculator.java
+++ src/br/pratica/iniciante/level1/SimpleCalculator.java
@@ -1,4 +1,39 @@
 package br.pratica.iniciante.level1;
 
+import java.util.Scanner;
+
 public class SimpleCalculator {
+    public static void main(String[] args) {
+        int resultado;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*****Calculadora Simples*****" +
+                "\n↓↓↓Opções↓↓↓" +
+                "\n1 - Soma" +
+                "\n2 - Subtrair" +
+                "\n3 - Multiplicar" +
+                "\n4 - Dividir" +
+                "\nOBS: SÓ É POSSIVEL FAZER UMA FUNÇÃO POR VEZ!" +
+                "\n");
+        System.out.println("Digite o primeiro numero: ");
+        int n1 = scanner.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int n2 = scanner.nextInt();
+        System.out.println("Qual função deseja seguir com os numero " + n1 + " e " + n2 + " que informou: ");
+        int opção = scanner.nextInt();
+        if (opção == 1) {
+            resultado = n1 + n2;
+            System.out.println("A soma dos " + n1 + " + " + n2 + " = " + resultado);
+        }else if (opção == 2) {
+            resultado = n1 - n2;
+            System.out.println("A subtração dos " + n1 + " - " + n2 + " = " + resultado);
+        } else if (opção == 3) {
+            resultado = n1 * n2;
+            System.out.println("A multiplicação dos " + n1 + " * " + n2 + " = " + resultado);
+        } else if (opção == 4){
+            resultado = n1 / n2;
+            System.out.println("A divisão dos " + n1 + " / " + n2 + " = " + resultado);
+        } else {
+            System.out.println("ERRO! numero invalido tentar rodar novamente");
+        }
+    }
 }
